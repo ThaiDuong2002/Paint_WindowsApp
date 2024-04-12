90bb17c1caaf3cf16fb2869a08d044f213f5f582
ThaiDuong2002 <105373894+ThaiDuong2002@users.noreply.github.com>
2024-04-13 00:34:28 +0700

    UI Done

--- PaintProject/MainWindow.xaml.cs
+++ PaintProject/MainWindow.xaml.cs
@@ -5,9 +5,20 @@
     /// </summary>
     public partial class MainWindow : Fluent.RibbonWindow
     {
+        private Options options = new Options();
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void RibbonWindow_Loaded(object sender, System.Windows.RoutedEventArgs e)
+        {
+            sizeComboBox.ItemsSource = options.Sizes;
+            sizeComboBox.SelectedIndex = 1;
+            outlineComboBox.ItemsSource = options.Outlines;
+            outlineComboBox.SelectedIndex = 0;
+            solidColorsListView.ItemsSource = options.Colors;
+            solidColorsListView.SelectedIndex = 0;
         }
     }
 }
---
+++ PaintProject/Options.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PaintProject
+{
+    public class SizeOption
+    {
+        public required string Label { get; set; }
+        public int Value { get; set; }
+        public FontWeight FontWeight { get; set; }
+    }
+    public class OutlineOption
+    {
+        public required DoubleCollection Value { get; set; }
+    }
+    public class Options
+    {
+        public ObservableCollection<SizeOption> Sizes { get; set; }
+        public ObservableCollection<OutlineOption> Outlines { get; set; }
+        public ObservableCollection<SolidColorBrush> Colors { get; set; }
+        public Options()
+        {
+            Colors = new ObservableCollection<SolidColorBrush>
+            {
+                Brushes.Black,
+                Brushes.Red,
+                Brushes.Orange,
+                Brushes.Yellow,
+                Brushes.Green,
+                Brushes.Blue,
+                Brushes.Indigo,
+                Brushes.Violet,
+                Brushes.White,
+                Brushes.Gray,
+            };
+            Sizes = new ObservableCollection<SizeOption>
+               {
+                new SizeOption { Label = "1px", Value = 1, FontWeight = FontWeights.Light },
+                new SizeOption { Label = "3px", Value = 3, FontWeight = FontWeights.Normal },
+                new SizeOption { Label = "5px", Value = 5, FontWeight = FontWeights.Medium },
+                new SizeOption { Label = "8px", Value = 8, FontWeight = FontWeights.Bold }
+            };
+            Outlines = new ObservableCollection<OutlineOption>
+            {
+                new OutlineOption { Value = new DoubleCollection() },
+                new OutlineOption { Value = new DoubleCollection { 1, 1 } },
+                new OutlineOption { Value = new DoubleCollection { 1, 6 } },
+                new OutlineOption { Value = new DoubleCollection { 6, 1 } },
+                new OutlineOption { Value = new DoubleCollection { 4, 1, 1, 1, 1, 1 } },
+                new OutlineOption { Value = new DoubleCollection { 5, 5, 1, 5 } },
+                new OutlineOption { Value = new DoubleCollection { 1, 2, 4 } },
+                new OutlineOption { Value = new DoubleCollection { 4, 2, 4 } },
+                new OutlineOption { Value = new DoubleCollection { 4, 2, 1, 2 } },
+                new OutlineOption { Value = new DoubleCollection { 4, 2, 1, 2, 1, 2 } },
+            };
+        }
+    }
+}
